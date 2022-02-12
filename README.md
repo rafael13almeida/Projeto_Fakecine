c8b5690eb67dbf0e7b09e3e8a349771bf5b5545f
Rafael Almeida <rafael13almeida@gmail.com>
2022-02-12 19:16:33 -0300

    Inclusão do inicio do Projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto usando PHP, simulando uma forma de stream de vídeos.
+Projeto usando framework Css Materialize.  
+# Projeto_Fakecine
---
+++ galeria.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS Materialize !-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!--JS Materialize !-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!--Fonte para os ícones!-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Fakecine</title>
+</head>
+
+<body>
+    <nav class="nav-extended blue-grey lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>FAKECINE</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent blue-grey darken-3">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/zAC6fCnaOHMpPFpOpngTSmTDnLK.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9.8
+                    </p>
+                    <span class="card-title ">Encanto</span>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/g0UPcVwhRtlLhNRowM324o7UJ1f.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9.3
+                    </p>
+                    <span class="card-title ">Sing 2</span>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/lBhAvhY1Wk36qICfrizBbat7MkP.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9.9
+                    </p>
+                    <span class="card-title ">O Rei Leão</span>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/nFuxY9pebpnkXaZ2H06qX68eIEy.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 8.9
+                    </p>
+                    <span class="card-title ">Raya</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
